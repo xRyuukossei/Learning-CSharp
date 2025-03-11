42a04c81afcdd02b9131dfd33cd9d5d0d35f0e6d
Ryuukossei <gui_dias.freitas@hotmail.com>
2025-03-10 22:04:11 -0300

    How to Casting

---
+++ DevSuperior/Casting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 1.2f;
+
+            Console.WriteLine("Example with Floating Number: " + a);
+
+            double b = a;
+
+            Console.WriteLine("Implicit Conversion: " + b);
+
+            int c = (int)b;
+
+            Console.WriteLine("Explicit Conversion:  " + c);
+        }
+    }
+}
