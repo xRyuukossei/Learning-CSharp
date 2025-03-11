ec66e50c44f2da753d85e380109091d13629d1c0
Ryuukossei <gui_dias.freitas@hotmail.com>
2025-03-10 22:05:55 -0300

    add Data Types

---
+++ DevSuperior/InputData/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace InputData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your full name: ");
+            string fullName = Console.ReadLine();
+
+            Console.WriteLine("How many bedrooms are there in your house? ");
+            int bedrooms = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the price of a product: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter your last name, age, height (all on the same line): ");
+            string[] vet = Console.ReadLine().Split(' ');
+            string lastName = vet[0];
+            int age = int.Parse(vet[1]);
+            double height = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            
+            Console.WriteLine(fullName);
+            Console.WriteLine(bedrooms);
+            Console.WriteLine(price.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ DevSuperior/OutputData/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace OutputData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product1 = "Desktop";
+            string product2 = "Office Desk";
+
+            byte age = 30;
+            int code = 5290;
+            char gender = 'M';
+
+            double price1 = 2100.0;
+            double price2 = 650.50;
+            double average = 53.234567;
+
+            Console.WriteLine("Products:");
+            Console.WriteLine("{0}, priced at: $ {1}", product1, price1);
+            Console.WriteLine($"{product2}, priced at: $ {price2}");
+            Console.WriteLine();
+            Console.WriteLine("Registration: " + age + " years old, code " + code + " and gender: " + gender);
+            Console.WriteLine();
+            Console.WriteLine("Measure with eight decimal places: {0:F8}", average);
+            Console.WriteLine($"Rounded (Three decimal places): {average:F3}");
+            Console.WriteLine("Invariant culture decimal separator: " + average.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
