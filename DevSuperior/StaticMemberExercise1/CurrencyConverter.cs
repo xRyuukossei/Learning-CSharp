50ec4581cf201ebad1bf72e213bd0fcfb79e9b79
Ryuukossei <gui_dias.freitas@hotmail.com>
2025-03-17 17:11:34 -0300

    add Static Member Exercise

---
+++ DevSuperior/StaticMemberExercise1/CurrencyConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace StaticMemberExercise1;
+public static class CurrencyConverter
+{
+    public static double Iof = 1.06;
+
+    public static double DolToBrl(double dolar, double quantity)
+    {
+        return quantity * dolar * Iof;
+    }
+}
---
+++ DevSuperior/StaticMemberExercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace StaticMemberExercise1
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            Console.Write("What is the exchange rate of the dollar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("How many dollars are you going to buy? ");
+            double real = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Amount to be paid in reais = " + CurrencyConverter.DolToBrl(dolar, real).ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
