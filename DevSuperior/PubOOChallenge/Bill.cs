b2dc04cff062fa31e17c817170cfdf3780e069c2
Ryuukossei <gui_dias.freitas@hotmail.com>
2025-03-19 16:02:55 -0300

    add PubOOChallenge

---
+++ DevSuperior/PubOOChallenge/Bill.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PubOOChallenge;
+public class Bill
+{
+    public char Gender;
+    public int Beer = 0;
+    public int Barbecue = 0;
+    public int SoftDrink = 0;
+
+    public double Feeding()
+    {
+        return Beer * 5.0 + Barbecue * 7.0 + SoftDrink * 3.0;
+    }
+
+    public double Cover()
+    {
+        if ( Feeding() < 30.0)
+        {
+            return 4.0;
+        }
+        else
+        {
+            return 0.0;
+        }
+    }
+
+    public double Ticket()
+    {
+        if ( Gender == 'F')
+        {
+            return 8.0;
+        }
+        else
+        {
+            return 10.0;
+        }
+    }
+
+    public double Total()
+    {
+        return Cover() + Feeding() + Ticket();
+    }
+}
---
+++ DevSuperior/PubOOChallenge/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PubOOChallenge
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            Bill bill = new Bill();
+
+            Console.Write("Gender: ");
+            bill.Gender = char.Parse(Console.ReadLine());
+            Console.Write("Number of Beers: ");
+            bill.Beer = int.Parse(Console.ReadLine());
+            Console.Write("Number of Soft Drinks: ");
+            bill.SoftDrink = int.Parse(Console.ReadLine());
+            Console.Write("Number of Barbecues: ");
+            bill.Barbecue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nREPORTS: ");
+            Console.WriteLine("Consumption = R$ " + bill.Feeding().ToString("F2"), CultureInfo.InvariantCulture);
+            if (bill.Cover() == 0.0)
+            {
+                Console.WriteLine("Exempt from cover charge");
+            }
+            else
+            {
+                Console.WriteLine("Cover Charge = R$ " + bill.Cover().ToString("F2"), CultureInfo.InvariantCulture);
+            }
+                       
+            Console.WriteLine("Ticket = R$ " + bill.Ticket().ToString("F2"), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("\nAmount to pay = R$ " + bill.Total().ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
