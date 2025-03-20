129bfc0fb9e2f5e611515f64457d2c8da58c5642
Ryuukossei <gui_dias.freitas@hotmail.com>
2025-03-19 21:19:32 -0300

    add exercise with Constructor and Encapsulation Concepts

---
+++ DevSuperior/ConstructorEncapsulationExercise/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConstructorEncapsulationExercise;
+internal class Account
+{
+    private string _name;
+    public int AccountNumber { get; private set; }
+    public double Balance { get; private set; }
+
+    public Account() {}
+    public Account(string name, int accountNumber) : this()
+    {
+        _name = name;
+        AccountNumber = accountNumber;
+    }
+
+    public Account(string name, int accountNumber, double balance) : this(name, accountNumber)
+    {
+        Balance = balance;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+        set { if (value != null && value.Length >= 3)
+            {
+                _name = value;
+            }
+        }
+    }
+
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+    }
+
+    public void Withdrawal(double amount)
+    {
+        Balance -= amount + 5;
+    }
+
+    public override string ToString()
+    {
+        return "Account " 
+            + AccountNumber 
+            + ", Holder: " 
+            + _name 
+            + ", Balance: $ " 
+            + Balance.ToString("F2", CultureInfo.InvariantCulture);
+    }
+}
---
+++ DevSuperior/ConstructorEncapsulationExercise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConstructorEncapsulationExercise
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            Account account;
+
+            Console.Write("Enter the account number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Enter the account holder: ");
+            string name = Console.ReadLine();
+            Console.Write("Will there be an initial deposit (y/n)? ");
+            char initialDeposit = char.Parse(Console.ReadLine());
+
+            if (initialDeposit == 'y')
+            {
+                Console.Write("Enter the initial deposit amount: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account = new Account(name, number, balance);
+            }
+            else
+            {
+                account = new Account(name, number);
+            }
+
+            Console.WriteLine("\nAccount details: ");
+            Console.WriteLine(account);
+
+            Console.Write("\nEnter a deposit amount: ");
+            account.Deposit(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Updated account details: ");
+            Console.WriteLine(account);
+
+            Console.Write("\nEnter a withdrawal amount: ");
+            account.Withdrawal(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Updated account details: ");
+            Console.WriteLine(account);
+        }
+    }
+}
