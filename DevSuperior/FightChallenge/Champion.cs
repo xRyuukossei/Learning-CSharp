6bb87eec24b5769910c3ee76a544e3b0a7328f2e
Ryuukossei <gui_dias.freitas@hotmail.com>
2025-03-20 07:47:15 -0300

    add FightChallenge

---
+++ DevSuperior/FightChallenge/Champion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FightChallenge;
+internal class Champion
+{
+    private string _name;
+    private int _attack;
+    private int _armor;
+    public int Life { get; private set; }
+
+    public Champion(string name, int attack, int armor, int life)
+    {
+        _name = name;
+        _attack = attack;
+        _armor = armor;
+        Life = life;
+    }
+    public void TakeDamage(int attack)
+    {
+        if (_armor >= attack)
+        {
+            Life -= 1;
+        }
+        else
+        {
+            Life -= (attack - _armor);
+        }
+    }
+    public string Status()
+    {
+        if (Life <= 0)
+        {
+            return _name + ": " + 0 + " health (died)";
+        }
+        else
+        {
+            return _name + ": " + Life + " health";
+        }
+    }
+}
---
+++ DevSuperior/FightChallenge/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FightChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first champion's data: ");
+            Console.Write("Name: ");
+            string name1 = Console.ReadLine();
+            Console.Write("Health: ");
+            int health1 = int.Parse(Console.ReadLine());
+            Console.Write("Attack: ");
+            int attack1 = int.Parse(Console.ReadLine());
+            Console.Write("Armor: ");
+            int armor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nEnter the second champion's data: ");
+            Console.Write("Name: ");
+            string name2 = Console.ReadLine();
+            Console.Write("Health: ");
+            int health2 = int.Parse(Console.ReadLine());
+            Console.Write("Attack: ");
+            int attack2 = int.Parse(Console.ReadLine());
+            Console.Write("Armor: ");
+            int armor2 = int.Parse(Console.ReadLine());
+
+            Champion champion1 = new Champion(name1, attack1, armor1, health1);
+            Champion champion2 = new Champion(name2, attack2, armor2, health2);
+
+            Console.Write("\nHow many turns do you want to execute? ");
+            int rounds = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= rounds; i++)
+            {
+                if (champion1.Life > 0 && champion2.Life > 0)
+                {
+                    champion1.TakeDamage(attack2);
+                    champion2.TakeDamage(attack1);
+                    Console.WriteLine($"\nTurn {i} result: ");
+                    Console.WriteLine(champion1.Status());
+                    Console.WriteLine(champion2.Status());
+                }
+            }
+            Console.WriteLine("\nEND OF COMBAT");
+        }
+    }
+}
