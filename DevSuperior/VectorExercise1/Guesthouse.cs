8a22e3095ceb45d3938dac11c0d330cbd697ea08
Ryuukossei <gui_dias.freitas@hotmail.com>
2025-03-25 19:55:53 -0300

    add Vector Exercises

---
+++ DevSuperior/VectorExercise1/Guesthouse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VectorExercise1
+{
+    internal class Guesthouse
+    {
+        private string _name;
+        private string _email;
+        private int _roomNumber;
+
+        public Guesthouse(string name, string email, int roomNumber)
+        {
+            _name = name;
+            _email = email;
+            _roomNumber = roomNumber;
+        }
+
+        public override string ToString()
+        {
+            return _roomNumber + ": " + _name + ", " + _email;
+        }
+    }
+}
---
+++ DevSuperior/VectorExercise1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace VectorExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Guesthouse[] guesthouses = new Guesthouse[10];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"\nRent #{i + 1}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+
+                guesthouses[room] = new Guesthouse(name, email, room);
+            }
+
+            Console.WriteLine("\nBusy rooms: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (guesthouses[i] != null)
+                {
+                    Console.WriteLine(guesthouses[i]);
+                }
+            }
+        }
+    }
+}
