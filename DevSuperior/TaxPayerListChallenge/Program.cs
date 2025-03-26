10f123fe65ced51618451e9629b651d27e0a5cd0
Ryuukossei <gui_dias.freitas@hotmail.com>
2025-03-26 13:15:10 -0300

    TaxPayerChallenge

---
+++ DevSuperior/TaxPayerListChallenge/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace TaxPayerListChallenge
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            Console.Write("How many taxpayers will you enter? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<TaxPayer> list = new List<TaxPayer>();
+
+            for (int i = 0; i < n; i++) 
+            {
+                Console.WriteLine("\nEnter the data for the " + n +"° taxpayer:");
+                Console.Write("Annual income from salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Annual income from service provision: ");
+                double serviceProvision = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Annual income from capital gains: ");
+                double capitalGains = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Medical expenses: ");
+                double medicalExpenses = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Educational expenses: ");
+                double educationalExpenses = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                list.Add( new TaxPayer(salary, serviceProvision, capitalGains, medicalExpenses, educationalExpenses));
+            }
+
+            foreach (TaxPayer taxPayer in list)
+            {
+                Console.WriteLine("\nSummary of the " + n + "° taxpayer:");
+                Console.WriteLine("Total gross tax: " + taxPayer.GrossTax().ToString("F2"), CultureInfo.InvariantCulture);
+                Console.WriteLine("Deduction: " + taxPayer.TaxRebate().ToString("F2"), CultureInfo.InvariantCulture);
+                Console.WriteLine("Tax due: " + taxPayer.NetTax().ToString("F2"), CultureInfo.InvariantCulture);
+
+            }
+        }
+    }
+}
---
+++ DevSuperior/TaxPayerListChallenge/TaxPayer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace TaxPayerListChallenge;
+internal class TaxPayer
+{
+    public double SalaryIncome { get; set; }
+    public double ServicesIncome { get; set; }
+    public double CapitalIncome { get; set; }
+    public double HealthSpending { get; set; }
+    public double EducationSpending { get; set; }
+
+    public TaxPayer(double salaryIncome, double servicesIncome, double capitalIncome, double healthSpending, double educationSpending)
+    {
+        SalaryIncome = salaryIncome;
+        ServicesIncome = servicesIncome;
+        CapitalIncome = capitalIncome;
+        HealthSpending = healthSpending;
+        EducationSpending = educationSpending;
+    }
+    public double SalaryTax()
+    {
+        double salaryIncomeMonth = (SalaryIncome / 12);
+        if (salaryIncomeMonth >= 3000.00 && salaryIncomeMonth < 5000.00) 
+        {
+            return SalaryIncome * 0.10;
+        }
+        else if (salaryIncomeMonth >= 5000.00)
+        {
+            return SalaryIncome * 0.20;
+        }
+        else
+        {
+            return 0.0;
+        }
+    }
+    public double ServicesTax()
+    {
+        return ServicesIncome * 0.15;
+    }
+    public double CapitalTax()
+    {
+        return CapitalIncome * 0.20;
+    }
+    public double GrossTax()
+    {
+        return SalaryTax() + ServicesTax() + CapitalTax();
+    }
+    public double TaxRebate()
+    {
+        double totalRebate = HealthSpending + EducationSpending;
+        if (totalRebate <= (GrossTax() * 0.3)) 
+        {
+            return totalRebate;
+        } 
+        else 
+        { 
+            return GrossTax() * 0.3; 
+        }
+    }
+    public double NetTax()
+    {
+        return GrossTax() - TaxRebate();
+    }
+}
