1e5d4ce2dfd45db0de0668ea1406856abb4961a5
Ryuukossei <gui_dias.freitas@hotmail.com>
2025-04-07 20:49:56 -0300

    Exceptions Exercise

---
+++ DevSuperior/ExceptionHandlingExercise1/Exceptions/WithdrawException.cs
@@ -0,0 +1,7 @@
+﻿namespace ExceptionHandlingExercise1.Exceptions;
+internal class WithdrawException : ApplicationException
+{
+    public WithdrawException(string message) : base(message) 
+    {
+    }
+}
---
+++ DevSuperior/ExceptionHandlingExercise1/Program.cs
@@ -0,0 +1,40 @@
+﻿using ExceptionHandlingExercise1.Entities;
+using ExceptionHandlingExercise1.Exceptions;
+using System;
+using System.Globalization;
+
+namespace ExceptionHandlingExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string name = Console.ReadLine();
+                Console.Write("Initical balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, name, balance, withdrawLimit);
+
+                Console.Write("\nEnter amount for withdraw: ");
+                double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.Withdraw(withdraw);
+
+                Console.WriteLine("New balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (WithdrawException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+
+        }
+    }
+}
